deb1ed6ccb4e3a9bffd2a0d33740c9fd7c49fd46
Falsi3007 <falsivadaliya@gmail.com>
2024-05-19 13:36:20 +0530

    update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,30 @@
-import express, { json } from 'express';
-import {PORT, DB_URL} from './secret.js'
+import express from 'express';
+import { PORT, mongoDBURL} from './secret.js';
 import mongoose from 'mongoose';
-import bookRouter from './routes/book_routes.js'
-import cors from 'cors'
+import booksRoutes from './routes/bookRoutes.js';
+import cors from 'cors';
 
 const app = express();
+
 app.use(express.json());
 
 app.use(cors())
 
-app.listen(PORT, ()=> {
-    console.log("Server is running on port 3000");
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send('Welcome to Express server')
 })
 
-app.get("/", (req,res) =>{
-    res.send("Server is runnning");
-} )
-
-app.use('/book',bookRouter)
+app.use('/books', booksRoutes);
 
 mongoose
-    .connect(DB_URL)
-    .then(()=>{console.log("DB is Connected");})
-    .catch((e)=> console.log(e))
-
-
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT , () => {
+            console.log("Server is running on port ${PORT}");
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
